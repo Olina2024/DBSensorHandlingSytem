d9a4e619cf28017288b02dbcf3d544b45db91b91
Olina2024 <ouyangjing1983@gmail.com>
2025-09-18 12:52:46 +0200

    backend

--- src/main/java/com/example/thymeleafDemo/Resposity/SensorRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.thymeleafDemo.Resposity;
-
-import com.example.thymeleafDemo.Model.Sensor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SensorRepo extends JpaRepository<Sensor,Integer> {
-
-}
