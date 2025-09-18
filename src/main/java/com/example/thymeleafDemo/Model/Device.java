43c75824d00faecc08be10594416c496d992d660
Olina2024 <ouyangjing1983@gmail.com>
2025-09-18 12:53:26 +0200

    "trying to merge from 'Jing' to 'Amelea'"

--- src/main/java/com/example/thymeleafDemo/Model/Device.java
+++ src/main/java/com/example/thymeleafDemo/Model/Device.java
@@ -1,4 +1,35 @@
 package com.example.thymeleafDemo.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="Device")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Device {
+    @Id
+    @GeneratedValue
+    protected  Long device_id;
+    protected  String device_type;
+
+    @OneToMany(mappedBy = "device", cascade = CascadeType.ALL, orphanRemoval = true)
+
+    private List<Sensor> sensors;
+
+    public Device(String device_type, List<Sensor> sensors)
+    {
+        for (Sensor sensor : sensors)
+            System.out.println(sensor.getSensor_type()+sensor.getValue());
+        this.sensors=sensors;
+
+        this.device_type=device_type;
+    }
+
+
 }
