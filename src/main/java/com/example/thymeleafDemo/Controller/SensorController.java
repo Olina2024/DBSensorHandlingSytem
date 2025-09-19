67c701def012421cb9b2396b557b2657b98cd696
Amelea <amelea.nordfors@yh.nackademin.se>
2025-09-19 12:37:43 +0200

    first simple frontends!

--- src/main/java/com/example/thymeleafDemo/Controller/SensorController.java
+++ src/main/java/com/example/thymeleafDemo/Controller/SensorController.java
@@ -1,9 +1,30 @@
 package com.example.thymeleafDemo.Controller;
 
 
+import com.example.thymeleafDemo.Resposity.SensorRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
 public class SensorController {
+
+    private SensorRepo sensorRepo;
+
+    private SensorController(SensorRepo sensorRepo) {this.sensorRepo = sensorRepo;}
+
+    @RequestMapping("StartHere")
+    public String StartHere() {
+        return "start.html";
+    }
+
+    // välj mellan senaste och historiska mätningar
+    @PostMapping("showDevice")
+    @ResponseBody
+    public String pageTwo() {
+        return "Steg två\nHär lägger vi in två alternativ";
+    }
 }
