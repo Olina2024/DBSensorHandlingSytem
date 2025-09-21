1a51093da2ce84ff12b6ac4b4374dc31ac8075a4
Olina2024 <ouyangjing1983@gmail.com>
2025-09-21 17:26:36 +0200

    döp om sensor namn till

--- src/main/java/com/example/thymeleafDemo/Resposity/SensorDataRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.thymeleafDemo.Resposity;
-import com.example.thymeleafDemo.Model.SensorData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SensorDataRepo extends JpaRepository<SensorData, Long> {
-}
